5f8d998b8e7599c10f7b4982e56b15973531e64a
Steve Gkountouvas <stgountouvas@gmail.com>
2021-01-02 19:15:21 +0200

    Restructured project

--- seq2seq/utils/__init__.py
+++ seq2seq/utils/__init__.py
@@ -1,11 +1,13 @@
+import csv
+
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 
 from .constants import CHARACTER_START_TOKEN, CHARACTER_END_TOKEN, CHARACTER_PAD_TOKEN, WORD_END_TOKEN, WORD_START_TOKEN, WORD_PAD_TOKEN
-from .preprocessing import unicode_to_ascii, remove_numbers_from_texts, unicode_to_ascii_from_texts, add_space_between_word_punctuation, reverse_one_hot_character_level, reverse_one_hot_word_level, \
-	reverse_tokenization_character_level, reverse_tokenization_word_level
+from .helpers import reverse_one_hot_character_level, reverse_one_hot_word_level, reverse_tokenization_character_level, reverse_tokenization_word_level, file_to_waveform
+from .text_preprocessing import unicode_to_ascii, remove_numbers_from_texts, unicode_to_ascii_from_texts, add_space_between_word_punctuation
 
 
 def load_data(path, lines_limit=None, max_length=None, remove_duplicates=True, remove_in_list=None):
@@ -183,20 +185,6 @@ def prepare_for_translation(sentences, input_tokenizer, max_encoder_seq_length, 
 	return sequences
 
 
-def one_hot_map(input_data, target_data, input_vocab_size, output_vocab_size):
-	encoder_input_data = tf.one_hot(input_data[0], depth=input_vocab_size)
-	decoder_input_data = tf.one_hot(input_data[1], depth=output_vocab_size)
-	decoder_target_data = tf.one_hot(target_data, depth=output_vocab_size)
-	return (encoder_input_data, decoder_input_data), decoder_target_data
-
-
-def one_hot_target_data_map(input_data, target_data, output_vocab_size):
-	encoder_input_data = input_data[0]
-	decoder_input_data = input_data[1]
-	decoder_target_data = tf.one_hot(target_data, depth=output_vocab_size)
-	return (encoder_input_data, decoder_input_data), decoder_target_data
-
-
 def reverse_one_hot(sequences, tokenizer, character_level):
 	if character_level:
 		result = reverse_one_hot_character_level(sequences, tokenizer)
@@ -217,3 +205,88 @@ def reverse_tokenization(sequences, tokenizer, character_level):
 	if len(result) == 1:
 		result = result[0]
 	return result
+
+
+#######
+
+def read_tsv(dataset_path, tsv_name):
+	data = []
+	with open(dataset_path + tsv_name, encoding="utf-8") as train_tsc:
+		reader = csv.reader(train_tsc, delimiter="\t", strict=True)
+		next(reader)  # skip header
+		for row in reader:
+			file_name = row[1].replace(".mp3", ".wav")
+			sentence = row[2]
+			data.append((file_name, sentence))
+	return data
+
+
+def load_tsv(dataset_path, tsv_data, character_level, to_lower, to_ascii, min_duration=None, max_duration=None, bitrate=16000):
+	waveforms = []  # encoder_inputs
+	labels = []
+	for row in tsv_data:
+		filename = row[0]
+		label = row[1].strip()
+		waveform = file_to_waveform(dataset_path + "clips/" + filename)
+		waveforms.append(waveform)
+		labels.append(label)
+
+	print("Total waveforms {0}".format(len(waveforms)))
+	if min_duration:
+		filtered_labels = []
+		filtered_waveforms = []
+
+		for label, waveform in zip(labels, waveforms):
+			if waveform.shape[0] / bitrate > min_duration:
+				filtered_waveforms.append(waveform)
+				filtered_labels.append(label)
+		print("Removed {0} waveform with duration less than {1}".format(len(waveforms) - len(filtered_waveforms), min_duration))
+		waveforms = filtered_waveforms
+		labels = filtered_labels
+
+	if max_duration:
+		filtered_labels = []
+		filtered_waveforms = []
+
+		for label, waveform in zip(labels, waveforms):
+			if waveform.shape[0] / bitrate < max_duration:
+				filtered_waveforms.append(waveform)
+				filtered_labels.append(label)
+
+		print("Removed {0} waveform with duration more than {1}".format(len(waveforms) - len(filtered_waveforms), max_duration))
+		waveforms = filtered_waveforms
+		labels = filtered_labels
+
+	print("Loaded waveforms {0}".format(len(waveforms)))
+
+	encoder_input = pad_sequences(waveforms, padding='post', dtype="float32", value=0.0)
+
+	if to_ascii:
+		labels = unicode_to_ascii_from_texts(labels)
+
+	if character_level:
+		labels = [CHARACTER_START_TOKEN + label + CHARACTER_END_TOKEN for label in labels]
+
+		tokenizer = Tokenizer(filters='', char_level=character_level, lower=to_lower)
+		tokenizer.fit_on_texts(labels)
+
+		decoder_input = tokenizer.texts_to_sequences(labels)
+
+		decoder_input = pad_sequences(decoder_input, padding='post', value=tokenizer.word_index[CHARACTER_PAD_TOKEN])
+		decoder_target = np.concatenate((decoder_input[:, 1:], np.full((decoder_input.shape[0], 1), tokenizer.word_index[CHARACTER_PAD_TOKEN])), axis=1)
+
+	else:
+		labels = add_space_between_word_punctuation(labels)
+		labels = [WORD_START_TOKEN + " " + label + " " + WORD_END_TOKEN for label in labels]
+
+		tokenizer = Tokenizer(filters='', char_level=character_level, lower=to_lower)
+		tokenizer.fit_on_texts(labels)
+		tokenizer.fit_on_texts([WORD_PAD_TOKEN])
+
+		decoder_input = tokenizer.texts_to_sequences(labels)
+
+		decoder_input = pad_sequences(decoder_input, padding='post', value=tokenizer.word_index[WORD_PAD_TOKEN])
+		decoder_target = np.concatenate((decoder_input[:, 1:], np.full((decoder_input.shape[0], 1), tokenizer.word_index[WORD_PAD_TOKEN])), axis=1)
+
+	max_sentence_length = len(max(labels, key=len))
+	return (encoder_input, decoder_input, decoder_target), tokenizer, max_sentence_length
---
+++ seq2seq/utils/helpers.py
@@ -0,0 +1,134 @@
+import numpy as np
+import tensorflow as tf
+
+from .constants import WORD_START_TOKEN, WORD_END_TOKEN, WORD_PAD_TOKEN
+
+
+def file_to_waveform(filename):
+	audio_binary = tf.io.read_file(filename)
+	waveform, _ = tf.audio.decode_wav(audio_binary)
+	return tf.squeeze(waveform, axis=-1)
+
+
+def reverse_one_hot_character_level(sequences, tokenizer):
+	result = []
+
+	if len(sequences.shape) != 3:
+		sequences = [sequences]
+
+	for sentence in sequences:
+		out = ""
+		for char in sentence:
+			out = out + tokenizer.index_word[np.argmax(char)]
+		result.append(out.strip())
+
+	return result
+
+
+def reverse_one_hot_word_level(sequences, tokenizer):
+	result = []
+
+	if len(sequences.shape) != 3:
+		sequences = [sequences]
+
+	for sentence in sequences:
+		out = []
+		for word in sentence:
+			decoded_word = tokenizer.index_word[np.argmax(word)]
+			if decoded_word != WORD_START_TOKEN and decoded_word != WORD_END_TOKEN and decoded_word != WORD_PAD_TOKEN:
+				out.append(decoded_word)
+		result.append(" ".join(out))
+
+	return result
+
+
+def reverse_tokenization_character_level(sequences, tokenizer):
+	result = []
+
+	if len(sequences.shape) != 2:
+		sequences = [sequences]
+
+	for sentence in sequences:
+		out = ""
+		for char in sentence:
+			out = out + tokenizer.index_word[char]
+		result.append(out.strip())
+
+	return result
+
+
+def reverse_tokenization_word_level(sequences, tokenizer):
+	result = []
+
+	if len(sequences.shape) != 2:
+		sequences = [sequences]
+
+	for sentence in sequences:
+		out = []
+		for word in sentence:
+			decoded_word = tokenizer.index_word[word]
+			if decoded_word != WORD_START_TOKEN and decoded_word != WORD_END_TOKEN and decoded_word != WORD_PAD_TOKEN:
+				out.append(decoded_word)
+		result.append(" ".join(out))
+
+	return result
+
+
+def split_ds(ds: tf.data.Dataset, val_percentage=None, test_percentage=None, buffer_size=None):
+	val_percentage = val_percentage or 0
+	test_percentage = test_percentage or 0
+	buffer_size = buffer_size or 128 * 128
+	if val_percentage < 0 or val_percentage >= 1.0:
+		raise ValueError("val_percentage must be between (0,1)")
+	if test_percentage < 0 or test_percentage >= 1.0:
+		raise ValueError("test_percentage must be between (0,1)")
+	if (val_percentage + test_percentage) >= 1.0:
+		raise ValueError("val_percentage+test_percentage must be between (0,1)")
+
+	full_ds_size = len(ds)
+	print("Full size: {0}".format(full_ds_size))
+	if val_percentage == 0 and test_percentage == 0:
+		print("No split returning ds shuffled")
+		return ds.shuffle(buffer_size, reshuffle_each_iteration=False)
+	elif val_percentage != 0 and test_percentage == 0:
+		val_ds_size = int(full_ds_size * val_percentage)
+		train_ds_size = full_ds_size - val_ds_size
+
+		ds = ds.shuffle(buffer_size, reshuffle_each_iteration=False)
+
+		train_ds = ds.take(train_ds_size)
+		val_ds = ds.skip(train_ds_size)
+
+		print("Train size: {0}".format(len(train_ds)))
+		print("Val size: {0}".format(len(val_ds)))
+		return train_ds, val_ds
+
+	elif val_percentage == 0 and test_percentage != 0:
+		test_ds_size = int(full_ds_size * test_percentage)
+		train_ds_size = full_ds_size - test_ds_size
+
+		ds = ds.shuffle(buffer_size, reshuffle_each_iteration=False)
+
+		train_ds = ds.take(train_ds_size)
+		test_ds = ds.skip(train_ds_size)
+
+		print("Train size: {0}".format(len(train_ds)))
+		print("Test size: {0}".format(len(test_ds)))
+		return train_ds, test_ds
+	else:
+		val_ds_size = int(full_ds_size * val_percentage)
+		test_ds_size = int(full_ds_size * test_percentage)
+		train_ds_size = full_ds_size - test_ds_size - val_ds_size
+
+		ds = ds.shuffle(buffer_size, reshuffle_each_iteration=False)
+
+		train_ds = ds.take(train_ds_size)
+		remaining = ds.skip(train_ds_size)
+
+		test_ds = remaining.take(test_ds_size)
+		val_ds = remaining.skip(test_ds_size)
+
+		print("Train size: {0}".format(len(train_ds)))
+		print("Test size: {0}".format(len(test_ds)))
+		print("Val size: {0}".format(len(val_ds)))
+		return train_ds, val_ds, test_ds,
